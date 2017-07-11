010c9217e2cf65490290dfe8497e52938c44a6ef
Daniel Landau <daniel@landau.fi>
2017-07-11 10:13:58 +0300

    Extract function

--- index.js
+++ index.js
@@ -11,25 +11,35 @@ const container = process.env.DOCKER_CONTAINER || 'elegant_kowalevski'
 
 app.post('/license/url', textParser, (req, res, next) => {
   const url = req.body
-  request(url).then(body => {
-    const nomos = cp.exec(`docker exec -i ${container} bash "-c" \'f=$(tempfile); cat >$f; /usr/local/etc/fossology/mods-enabled/nomos/agent/nomos $f\'`, (error, nomosStdout, stderr) => {
-      const monk = cp.exec(`docker exec -i ${container} bash "-c" \'f=$(tempfile); cat >$f; /usr/local/etc/fossology/mods-enabled/monk/agent/monk $f\'`, (error, monkStdout, stderr) => {
-
-        const monkOutput = 'According to monk: ' + monkStdout
-        const nomosOutput = 'According to nomos: ' + nomosStdout
-        res.send(`
-${monkOutput}
-${nomosOutput}
-`)
-      })
-      monk.stdin.write(body)
-      monk.stdin.end()
-    })
-    nomos.stdin.write(body)
-    nomos.stdin.end()
-  }).catch(next)
+  request(url)
+    .then(body => analyzeFile(body))
+    .then(results => res.send(results))
+    .catch(next)
 })
 
 app.listen(3000, () => {
   console.log('Listening on 3000')
 })
+
+function analyzeFile(contents) {
+  const cmd = (agent) => `docker exec -i ${container} bash "-c" \'f=$(tempfile); cat >$f; /usr/local/etc/fossology/mods-enabled/${agent}/agent/${agent} $f\'`
+
+  return new Promise((resolve, reject) => {
+    const nomos = cp.exec(cmd('nomos'), (error, nomosStdout, stderr) => {
+      if (error) return reject(error)
+      const monk = cp.exec(cmd('monk'), (error, monkStdout, stderr) => {
+        if (error) return reject(error)
+        const monkOutput = 'According to monk: ' + monkStdout
+        const nomosOutput = 'According to nomos: ' + nomosStdout
+        return resolve(`
+${monkOutput}
+${nomosOutput}
+`)
+      })
+      monk.stdin.write(contents)
+      monk.stdin.end()
+    })
+    nomos.stdin.write(contents)
+    nomos.stdin.end()
+  })
+}
